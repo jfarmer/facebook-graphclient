3a8dfc6d8a4efc5904134a477ae531a99a4ee282
Brian Lopez <seniorlopez@gmail.com>
2010-05-06 09:06:29 -0700

    rescue and retry on host resolution and connection errors - facebook's api servers are less than reliable these days

--- lib/facebook-graphclient.rb
+++ lib/facebook-graphclient.rb
@@ -11,16 +11,16 @@ module Facebook
   GRAPH_URL = 'http://graph.facebook.com'
 
   class GraphClient
-  
+
     attr_reader :secret, :app_id
     attr_accessor :access_token
 
     def initialize facebook_settings = {}
       @app_id       = facebook_settings[:app_id]
       @secret       = facebook_settings[:secret]
-    
+
       @cookie = get_user_cookie facebook_settings[:cookies]
-    
+
       @access_token = facebook_settings[:access_token] || @cookie['access_token']
 
       @session = Patron::Session.new
@@ -44,17 +44,23 @@ module Facebook
 
     def api action, method, query_params = nil
       query_params[:access_token] ||= @access_token
-    
+
       query_string = '?' + query_params.map { |k,v| "#{k}=#{v}" }.join("&") unless query_params.empty?
-    
-      raw_response = @session.send(action, GRAPH_URL + method + query_string)
-    
+
+      begin
+        raw_response = @session.send(action, GRAPH_URL + method + query_string)
+      rescue Patron::HostResolutionError
+        retry
+      rescue Patron::ConnectionFailed
+        retry
+      end
+
       # TODO: Handle photo requests, which return photo data and not JSON
-    
+
       if raw_response.headers['Content-Type'] =~ /text\/javascript/
         # We have JSON
         response = Yajl::Parser.parse(raw_response.body)
-    
+
         if e = response['error']
           error = FacebookError.new(e['message'])
           error.data = e
@@ -65,12 +71,40 @@ module Facebook
       end
     end
 
+    def fql query
+      query_params = {
+        :access_token => @access_token,
+        :format => 'json',
+        :query => Rack::Utils.escape(query)
+      }
+
+      query_string = '?' + query_params.map { |k,v| "#{k}=#{v}" }.join("&") unless query_params.empty?
+
+      begin
+        raw_response = @session.get("https://api.facebook.com/method/fql.query" + query_string)
+      rescue Patron::HostResolutionError
+        retry
+      rescue Patron::ConnectionFailed
+        retry
+      end
+
+      response = Yajl::Parser.parse(raw_response.body)
+
+      if e = response.first['error']
+        error = FacebookError.new(e['message'])
+        error.data = e
+        raise error
+      else
+        response
+      end
+    end
+
     def valid?
       return false if @cookie.nil?
 
       unless @is_valid
         vars = @cookie.dup
-      
+
         good_sig = vars.delete 'sig'
         sig = Digest::MD5.hexdigest(vars.sort.map { |k,v| "#{k}=#{v}" }.compact.join + @secret)
 
